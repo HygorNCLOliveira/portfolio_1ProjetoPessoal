5a7264632561290a4aa03d7d9f3b689e5561399c
Hygor Oliveira <Hygor.o53@gmail.com>
2024-02-18 21:29:14 -0300

    feat: portfolio

---
+++ CODIGOS/JAVASCRIPT/script-Menu.js
@@ -0,0 +1,6 @@
+
+// Menu - mobile 
+const navbarBtn = document.querySelector(".navbarBtn");
+const navbar = document.querySelector(".navbar");
+
+navbarBtn.addEventListener("click", () => navbar.classList.toggle("active"));
